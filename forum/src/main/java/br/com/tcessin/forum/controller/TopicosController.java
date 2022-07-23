8c16c823d2fe18407852d1a4d3721e77c20728a7
thiagocessin <thiagoacessin@gmail.com>
2022-07-23 14:19:55 -0300

    Alterações

--- forum/src/main/java/br/com/tcessin/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/tcessin/forum/controller/TopicosController.java
@@ -1,25 +1,26 @@
 package br.com.tcessin.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tcessin.forum.controller.dto.TopicoDto;
-import br.com.tcessin.forum.modelo.Curso;
-import br.com.tcessin.forum.modelo.Topico;
+import br.com.tcessin.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
-		
-		Topico t = new Topico("Titulo 1", "Duvida conteúdo", new Curso("Java", "Programação"));
-				
-		return TopicoDto.converter(Arrays.asList(t,t,t));
+		return TopicoDto.converter(topicoRepository.findAll());
 	}
+	
 
 }
---
+++ forum/src/main/java/br/com/tcessin/forum/repository/TopicoRepository.java
@@ -0,0 +1,12 @@
+package br.com.tcessin.forum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.tcessin.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+	
+	
+	
+
+}
