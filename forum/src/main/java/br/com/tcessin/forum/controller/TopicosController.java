565a7aac3bd6e2e22c10419b00f445ed98c8f793
thiagocessin <thiagoacessin@gmail.com>
2022-07-23 14:52:11 -0300

    Criacao do endpoint post

--- forum/src/main/java/br/com/tcessin/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/tcessin/forum/controller/TopicosController.java
@@ -1,25 +1,51 @@
 package br.com.tcessin.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.tcessin.forum.controller.dto.TopicoDto;
+import br.com.tcessin.forum.controller.form.TopicoForm;
+import br.com.tcessin.forum.modelo.Topico;
+import br.com.tcessin.forum.repository.CursoRepository;
 import br.com.tcessin.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping(value = "/topicos")
 public class TopicosController {
 	
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
+	@Autowired
+	private CursoRepository cursoRepository;
 	
-	@RequestMapping("/topicos")
+	
+	@GetMapping
 	public List<TopicoDto> lista() {
 		return TopicoDto.converter(topicoRepository.findAll());
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		
+		Topico topico = form.toEntity(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+		
 	}
 	
 
---
+++ forum/src/main/java/br/com/tcessin/forum/controller/form/TopicoForm.java
@@ -0,0 +1,42 @@
+package br.com.tcessin.forum.controller.form;
+
+import br.com.tcessin.forum.modelo.Curso;
+import br.com.tcessin.forum.modelo.Topico;
+import br.com.tcessin.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	private String titulo;
+	private String mensagem;
+	private String nomeCurso;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public Topico toEntity(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+
+}
