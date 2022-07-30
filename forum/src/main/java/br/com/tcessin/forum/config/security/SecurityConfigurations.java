392b9e3e7be4c2ec11136722ebddb24154ceb7e0
thiagocessin <thiagoacessin@gmail.com>
2022-07-30 14:07:01 -0300

    Adicionando logica geracao do token jwt

---
+++ forum/src/main/java/br/com/tcessin/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,62 @@
+package br.com.tcessin.forum.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
+	
+	//método para configurar autenticação (usuários)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
+	
+	//configurações de autorização (perfil de acesso)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET,"/topicos").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
+			.anyRequest().authenticated()
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//"avisa" ao spring para não criar sessão, será usado stateless
+	}
+	
+	//configurações de recursos estáticos (JS, css, imagem)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+		
+	}
+	
+}
