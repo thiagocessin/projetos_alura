2cc16c9584860adbf745a4a46356744c5c5bb3c1
thiagocessin <thiagoacessin@gmail.com>
2022-08-03 08:57:43 -0300

    Adicionando autenticacao de usuario

--- forum/src/main/java/br/com/tcessin/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/tcessin/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -1,21 +1,29 @@
 package br.com.tcessin.forum.config.security;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.tcessin.forum.modelo.Usuario;
+import br.com.tcessin.forum.repository.UsuarioRepository;
 
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
 	
 	private TokenService tokenService;
+	private UsuarioRepository usuarioRepository;
 
-	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+	public AutenticacaoViaTokenFilter(TokenService tokenService,UsuarioRepository usuarioRepository) {
 		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
 	}
 
 	@Override
@@ -26,10 +34,24 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
 		
 		boolean valido = tokenService.isTokenValid(token);
 		
-		System.out.println(valido);
+		if(valido) {
+			autenticarCliente(token);
+		}
 		
 		filterChain.doFilter(request, response);
 		
+	}
+
+	private void autenticarCliente(String token) {
+
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
+
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+				usuario.getAuthorities());
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
--- forum/src/main/java/br/com/tcessin/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/tcessin/forum/config/security/SecurityConfigurations.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import br.com.tcessin.forum.repository.UsuarioRepository;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
@@ -23,6 +25,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	
 	//método para configurar autenticação (usuários)
@@ -44,7 +49,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated()
 			.and().csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//"avisa" ao spring para não criar sessão, será usado stateless
-			.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
+			.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService,usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	//configurações de recursos estáticos (JS, css, imagem)
