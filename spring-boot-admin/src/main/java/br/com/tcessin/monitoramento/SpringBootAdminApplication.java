e2e92a53a6d1d37c98fc42922bb0eac63fcefb8a
thiagocessin <thiagoacessin@gmail.com>
2022-08-05 13:40:54 -0300

    Configuracoes server admin

--- spring-boot-admin/src/main/java/br/com/tcessin/monitoramento/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/br/com/tcessin/monitoramento/SpringBootAdminApplication.java
@@ -1,9 +1,15 @@
 package br.com.tcessin.monitoramento;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
