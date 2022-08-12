43a681e86ff34b7a9bf507fbe4195effbb52c383
thiagocessin <thiagoacessin@gmail.com>
2022-08-12 13:46:09 -0300

    criacao do config server

--- ms-loja/src/main/java/br/com/tcessin/loja/MsLojaApplication.java
+++ ms-loja/src/main/java/br/com/tcessin/loja/MsLojaApplication.java
@@ -2,9 +2,19 @@ package br.com.tcessin.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MsLojaApplication {
+	
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsLojaApplication.class, args);
--- ms-loja/src/main/java/br/com/tcessin/loja/service/CompraService.java
+++ ms-loja/src/main/java/br/com/tcessin/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.tcessin.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,15 @@ import br.com.tcessin.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		RestTemplate client = new RestTemplate();
+	//	RestTemplate client = new RestTemplate();
 		
-		ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET,null, InfoFornecedorDTO.class);
+		ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET,null, InfoFornecedorDTO.class);
 		
 		System.out.println(response.getBody().getEndereco());
 		
