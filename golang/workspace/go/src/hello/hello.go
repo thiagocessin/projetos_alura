2b913f9c533404a785dfcfb909689eb888ed9ec1
thiagocessin <thiagoacessin@gmail.com>
2022-08-26 08:46:32 -0300

    Crianco ifs

--- golang/workspace/go/src/hello/hello.go
+++ golang/workspace/go/src/hello/hello.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	nome := "thiago"
+	versao := 1.1 // inferencia do nome var e tipo
+
+	fmt.Println("Hello ", nome)
+	fmt.Println("", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	//fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando")
+	} else if comando == 2 {
+		fmt.Println("Exibindo logs")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa")
+	} else {
+		fmt.Println("Comando não reconhecido")
+	}
+
 }
