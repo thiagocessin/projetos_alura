555238c3498a93f62f82da7acada55013b433ef0
thiagocessin <thiagoacessin@gmail.com>
2022-08-27 14:19:15 -0300

    criacao do monitoramento

--- golang/workspace/go/src/hello/hello.go
+++ golang/workspace/go/src/hello/hello.go
@@ -2,9 +2,48 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
+
+	exibeIntroducao()
+	for {
+		exibeMenu()
+
+		switch leComando() {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs")
+		case 0:
+			fmt.Println("Saindo..")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando não reconhecido")
+		}
+	}
+
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func leComando() int {
+
+	var comando int
+	//fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+
+	return comando
+
+}
+
+func exibeIntroducao() {
 
 	nome := "thiago"
 	versao := 1.1 // inferencia do nome var e tipo
@@ -12,22 +51,19 @@ func main() {
 	fmt.Println("Hello ", nome)
 	fmt.Println("", versao)
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do programa")
+}
 
-	var comando int
-	//fmt.Scanf("%d", &comando)
-	fmt.Scan(&comando)
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando")
 
-	if comando == 1 {
-		fmt.Println("Monitorando")
-	} else if comando == 2 {
-		fmt.Println("Exibindo logs")
-	} else if comando == 0 {
-		fmt.Println("Saindo do programa")
+	site := "https://www.alura.com.br"
+
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site carregado")
 	} else {
-		fmt.Println("Comando não reconhecido")
+		fmt.Println("Erro status:", resp.StatusCode)
 	}
 
 }
