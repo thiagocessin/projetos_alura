05690d4d7d76fb375d8584c2fd424467f11746b0
thiagocessin <thiagoacessin@gmail.com>
2022-09-22 13:42:54 -0300

    estrutura projeto GO

--- golang/workspace/go/src/banco/main.go
+++ golang/workspace/go/src/banco/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
+
+	fmt.Println(ContaCorrente{})
 
 }
