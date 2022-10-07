b59fe02a3f7f9d2adc14a770a99e9727c4adc050
thiagocessin <thiagoacessin@gmail.com>
2022-10-07 15:48:41 -0300

    Alterações

---
+++ mudi/src/main/java/br/com/tcessin/mudi/controller/PedidoController.java
@@ -0,0 +1,39 @@
+package br.com.tcessin.mudi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.tcessin.mudi.dto.RequisicaoNovoPedido;
+import br.com.tcessin.mudi.model.Pedido;
+import br.com.tcessin.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	
+	@GetMapping("formulario")
+	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
+		
+		return "pedido/formulario";
+		
+	}
+	
+	
+	
+}
