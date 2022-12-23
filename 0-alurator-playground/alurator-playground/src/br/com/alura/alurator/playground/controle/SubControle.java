32ecb86fbff3b320bc57175250bdd6901fd89a0c
thiagocessin <thiagoacessin@gmail.com>
2022-12-23 08:57:31 -0300

    novos testes java reflection

--- 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/controle/SubControle.java
+++ 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/controle/SubControle.java
@@ -3,7 +3,9 @@ package br.com.alura.alurator.playground.controle;
 public class SubControle extends Controle {
 
 	
-	private SubControle() {}
+	private SubControle(){
+		
+	}
 	
 	private SubControle(String s) {}
 	
--- 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstaciaObjetoCorretamente.java
+++ 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstaciaObjetoCorretamente.java
@@ -1,31 +1,46 @@
 package br.com.alura.alurator.playground.reflexao;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-
-import br.com.alura.alurator.playground.controle.SubControle;
 
 public class TesteInstaciaObjetoCorretamente {
 
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-		
-		
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, IllegalArgumentException, NoSuchMethodException, SecurityException {
+
+		// teste new Instance
+		Class<?> controle1 = Class.forName("br.com.alura.alurator.playground.controle.Controle");
+
+		try {
+			controle1.getDeclaredConstructor().newInstance();
+		} catch (InvocationTargetException e) {
+			
+			Throwable targetException = e.getTargetException();
+			System.out.println(targetException);
+			e.printStackTrace();
+		}
+
+		// controle1.newInstance();
+
+	}
+
+	
+	/*
+	private static void m1() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
 		Class<SubControle> subControleClasse1 = SubControle.class;
 		Class<?> subControleClasse2 = Class.forName("br.com.alura.alurator.playground.controle.SubControle");
 		Class<?> controleClasse2 = Class.forName("br.com.alura.alurator.playground.controle.Controle");
-		
-		
+
 		Constructor<SubControle> constructorSubControle = subControleClasse1.getDeclaredConstructor();
-		
+
 		System.out.println(constructorSubControle);
-		
+
 		constructorSubControle.setAccessible(true);
 		SubControle subControle = constructorSubControle.newInstance();
-		
-		System.out.println(subControle);
-		
-		
-		
-	}
 
+		System.out.println(subControle);
+
+	}
+*/
 }
