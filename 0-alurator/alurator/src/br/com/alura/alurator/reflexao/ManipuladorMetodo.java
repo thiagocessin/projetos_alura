98bf6c388fac9db0afd3da844cefd53806e76336
thiagocessin <thiagoacessin@gmail.com>
2023-01-05 08:44:41 +0044

    Novasm implementações

--- 0-alurator/alurator/src/br/com/alura/alurator/reflexao/ManipuladorMetodo.java
+++ 0-alurator/alurator/src/br/com/alura/alurator/reflexao/ManipuladorMetodo.java
@@ -2,20 +2,31 @@ package br.com.alura.alurator.reflexao;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class ManipuladorMetodo {
 
 	private Object instancia;
 	private Method metodo;
+	private Map<String, Object> mapParams;
 
-	public ManipuladorMetodo(Object instancia, Method metodo) {
+	public ManipuladorMetodo(Object instancia, Method metodo, Map<String, Object> mapParams) {
 		this.instancia = instancia;
 		this.metodo = metodo;
+		this.mapParams = mapParams;
 	}
 
 	public Object invocar() {
 		try {
-			return metodo.invoke(instancia);
+			
+			List<Object> listaParametros = new ArrayList<>();
+			Stream.of(metodo.getParameters())
+				.forEach(p -> listaParametros.add(mapParams.get(p.getName())));
+			
+			return metodo.invoke(instancia, listaParametros.toArray());
 		} catch (IllegalAccessException | IllegalArgumentException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
--- 0-alurator/alurator/src/br/com/alura/alurator/reflexao/ManipuladorObjeto.java
+++ 0-alurator/alurator/src/br/com/alura/alurator/reflexao/ManipuladorObjeto.java
@@ -2,6 +2,7 @@ package br.com.alura.alurator.reflexao;
 
 import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class ManipuladorObjeto {
 
@@ -14,15 +15,28 @@ public class ManipuladorObjeto {
 	}
 
 	public ManipuladorMetodo getMetodo(String nomeMetodo, Map<String, Object> params) {
-		
-		try {
-			Method metodo = instancia.getClass().getDeclaredMethod(nomeMetodo);
-			return new ManipuladorMetodo(instancia,metodo);
-		} catch (NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		
+
+		Stream<Method> metodos = Stream.of(instancia.getClass().getDeclaredMethods());
+
+		Method metodoSelecionado = metodos.filter(m -> filterMetodo(m, nomeMetodo, params)).findFirst()
+				.orElseThrow(() -> new RuntimeException("Metodo não encontrado"));
+
+		return new ManipuladorMetodo(instancia, metodoSelecionado, params);
+	}
+	
+	private boolean filterMetodo(Method metodo, String nomeMetodo, Map<String, Object> mapParametros) {
+
+		return metodo.getName().equals(nomeMetodo) 
+				&& metodo.getParameterCount() == mapParametros.values().size()
+				&& Stream.of(metodo.getParameters())
+							.allMatch(
+									parametroMetodo -> {
+										
+										System.out.println("NOME ARGUMENTO: " + parametroMetodo);
+										
+										return mapParametros.keySet().contains(parametroMetodo.getName()) 
+										&& mapParametros.get(parametroMetodo.getName()).getClass().equals(parametroMetodo.getType());});
+
 	}
 
 }
