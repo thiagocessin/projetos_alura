7f494838f9e73e7c4eb701932f8f841fd076149f
thiagocessin <thiagoacessin@gmail.com>
2023-01-11 08:17:50 +0044

    Testes

--- 0-estoque-api/estoque-api/src/br/com/alura/estoque/controle/ProdutoController.java
+++ 0-estoque-api/estoque-api/src/br/com/alura/estoque/controle/ProdutoController.java
@@ -7,29 +7,27 @@ import br.com.alura.estoque.dao.ProdutoDaoMock;
 import br.com.alura.estoque.modelo.Produto;
 
 public class ProdutoController {
-	
+
 	private ProdutoDaoMock produtoDao;
 
 	public ProdutoController() {
 		produtoDao = new ProdutoDaoMock();
 	}
-	
+
 	public List<Produto> lista() {
 		return produtoDao.lista();
 	}
-	
+
 	public List<Produto> filtra(String nome) {
 		return produtoDao.lista().stream()
-							.filter(produto -> produto.getNome().toLowerCase().startsWith(nome.toLowerCase()))
-							.collect(Collectors.toList());
+				.filter(produto -> produto.getNome().toLowerCase().startsWith(nome.toLowerCase()))
+				.collect(Collectors.toList());
 	}
-	
+
 	public List<Produto> filtra(String nome, String marca) {
 		return produtoDao.lista().stream()
-							.filter(produto -> 
-								produto.getNome().toLowerCase().startsWith(nome.toLowerCase())
-								&& produto.getMarca().equalsIgnoreCase(marca)
-							)
-							.collect(Collectors.toList());
+				.filter(produto -> produto.getNome().toLowerCase().startsWith(nome.toLowerCase())
+						&& produto.getMarca().equalsIgnoreCase(marca))
+				.collect(Collectors.toList());
 	}
 }
