d5d702c2c4bcd10a428f4e876a8f182761a8cb41
thiagocessin <thiagoacessin@gmail.com>
2023-01-18 08:56:36 +0044

    Criando container ioc

--- 0-alurator/alurator/src/br/com/alura/alurator/ioc/ContainerIoC.java
+++ 0-alurator/alurator/src/br/com/alura/alurator/ioc/ContainerIoC.java
@@ -4,12 +4,16 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 public class ContainerIoC {
 
+	private Map<Class<?>, Class<?>> mapaDeTipos = new HashMap<>();
+	
 	public Object getInstancia(Class<?> tipoFonte) {
 		Stream<Constructor<?>> construtores = Stream.of(tipoFonte.getDeclaredConstructors());
 
